dbafa8c5fc567a5c62ae1d289761de10586bc569
tetthys <jeongwoo0830z@gmail.com>
2024-05-30 21:55:38 +0900

    integrate scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div>App</div>
+      <p className="text text--red">Hello World</p>
+      <p className="text text--green">Hello World</p>
+      <p className="text text--blue">Hello World</p>
+    </>
+  );
 };
 
 export default App;
